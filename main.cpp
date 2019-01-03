a74c9e70bab3fa5fd1b0c275b2b172da07ffd259
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-01-02 19:51:20 -0800

    lab3

--- main.cpp
+++ main.cpp
@@ -6,6 +6,9 @@
 *********************************************************************/
 #include <iostream>
 
+class Die {};
+class LoadedDie {};
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
