2a1c0a567087465b420ed09ac06305e74bf1e94b
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-01-02 20:04:56 -0800

    lab3

--- LoadedDie.hpp
+++ LoadedDie.hpp
@@ -2,4 +2,8 @@
 #ifndef LOADEDDIE_HPP
 #define LOADEDDIE_HPP
 
+class LoadedDie : public Die {
+
+};
+
 #endif
--- main.cpp
+++ main.cpp
@@ -5,25 +5,6 @@
 **                  asdf
 *********************************************************************/
 #include <iostream>
-#include <ctime>
-
-class Die {
-private:
-    int N;                  // number of sides on the die
-public:
-    int randomInt() {       // returns a random integer
-        unsigned seed;
-        int randomNum;
-        seed = static_cast<unsigned int>(time(nullptr));
-        srand(seed);
-        randomNum = rand() % N + 1;
-        return randomNum;
-    }
-};
-
-class LoadedDie : public Die {
-
-};
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
