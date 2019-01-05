d1e44be539d64a52a2bc0b4f5dc7f2eb19e745d4
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-01-05 00:25:14 -0800

    lab3

--- main.cpp
+++ main.cpp
@@ -59,7 +59,6 @@ int main() {
                 // players roll dice and store results in game.p1Score or p2Score
                 int p1RollScore = 0, p2RollScore = 0;
 
-
                 // get random integer for each player
                 for (int player = 0; player < 2; player++) {
                     if (game.getDiceType(player) == NORMAL) {
