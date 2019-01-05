8629d67a59439b6d1d4b547f5e615b7d5628908c
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-01-05 00:20:46 -0800

    lab3

--- main.cpp
+++ main.cpp
@@ -83,21 +83,6 @@ int main() {
                 // update winner score
                 winner = game.updatePlayerScore(p1RollScore, p2RollScore);
 
-//                if (p1RollScore == p2RollScore) {
-//                    winner = "No player wins the round\n";
-//                }
-//                else if (p1RollScore > p2RollScore) {
-//                    winner = "Player one wins the round!\n";
-//                    game.addplayerOneScore();
-//                }
-//                else if (p1RollScore < p2RollScore) {
-//                    winner = "Player two wins the round!\n";
-//                    game.addplayerTwoScore();
-//                }
-//                else {
-//                    cout << "error calculating which player roll is higher!\n";
-//                }
-
                 // display game results after each round
                 game.submenueReport(winner, p1RollScore, p2RollScore, round+1);
 
